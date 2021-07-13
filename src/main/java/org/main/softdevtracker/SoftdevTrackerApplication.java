26d3480cbf8b79b18b5b7307471880cca46c66d1
iCharlieAraiza <eliptikman1234@gmail.com>
2021-07-12 19:08:54 -0500

    Init commit

---
+++ src/main/java/org/main/softdevtracker/SoftdevTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.main.softdevtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftdevTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftdevTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/main/softdevtracker/SoftdevTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.main.softdevtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftdevTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
